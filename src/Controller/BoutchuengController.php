8d9a74f2ff6d97fddaec2068168b816ace529987
Boutchueng <franck.boutchueng@etu.univ-st-etienne.fr>
2022-02-02 18:00:51 +0100

    créeation

--- src/Controller/BoutchuengController.php
+++ src/Controller/BoutchuengController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\Request;
 class BoutchuengController extends AbstractController
 {
     /**
@@ -18,3 +18,4 @@ class BoutchuengController extends AbstractController
         ]);
     }
 }
+
