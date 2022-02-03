c9b21b5b0108abf62ed43577c36515a39f5ee1ed
Boutchueng <franck.boutchueng@etu.univ-st-etienne.fr>
2022-02-03 09:59:45 +0100

    rajout lien base de donnée controller/form

--- src/Controller/BoutchuengController.php
+++ src/Controller/BoutchuengController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+
+
 class BoutchuengController extends AbstractController
 {
     /**
@@ -15,6 +18,24 @@ class BoutchuengController extends AbstractController
     {
         return $this->render('boutchueng/index.html.twig', [
             'controller_name' => 'BoutchuengController',
+        ]);
+    }
+
+
+    /**
+     * @Route("/traitement", name="traitement")
+     */ 
+    public function traitement(Request $request, EntityManagerInterface $manager ): Response
+    {
+        $pseudo = $request -> request -> get("pseudo") ;
+        $password = $request -> request -> get("pass") ;
+        
+        
+
+        return $this->render('boutchueng/traitement.html.twig', [
+            'controller_name' => 'BoutchuengController',
+            'pseudo'=>$pseudo,
+            'pass'=>$password,
         ]);
     }
 }
