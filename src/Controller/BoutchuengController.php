a87ab6d78fcf4208531f4d8a53c190e9d5c79fd9
Boutchueng <franck.boutchueng@etu.univ-st-etienne.fr>
2022-02-08 12:02:55 +0100

    final new util

--- src/Controller/BoutchuengController.php
+++ src/Controller/BoutchuengController.php
@@ -7,6 +7,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Utilisateur;
+
 
 
 class BoutchuengController extends AbstractController
@@ -29,12 +31,42 @@ class BoutchuengController extends AbstractController
     {
         $pseudo = $request -> request -> get("pseudo") ;
         $password = $request -> request -> get("pass") ;
+        $pseudoV = $manager -> getRepository(Utilisateur::class) -> findOneBy([ "Login" => $pseudo]);
+        if ($pseudoV ==NULL)
+            $msg ='cette est utilisateur inconnus';
+        elseif ($password == $pseudoV -> getPassword()){
+            $msg= 'Les informations sont correctes';
+        }
         
+        else{
+            $msg = 'ces informations ne correspondent pas';
+        }
         
-
-        return $this->render('boutchueng/traitement.html.twig', [
+         return $this->render('boutchueng/traitement.html.twig', [
             'controller_name' => 'BoutchuengController',
             'pseudo'=>$pseudo,
+            'pass'=>$password,
+            'message'=>$msg,
+            'loginV' =>$pseudoV,
+        ]);
+    }
+      /**
+     * @Route("/New_users", name="New_users")
+     */ 
+    public function New_users(Request $request,  EntityManagerInterface $manager): Response
+    {
+        $Login = $request -> request -> get("New_users");
+        $password= $request -> request ->get("pass");
+        $nom = new Utilisateur;
+        $nom -> setLogin($Login);
+        $nom-> setPassword($password);
+        $manager -> persist($nom);
+        $manager -> flush ();
+
+
+        return $this->redirectToRoute('boutchueng/new_util.html.twig', [
+            'controller_name' => 'BoutchuengController',
+            'nom'=>$nom,
             'pass'=>$password,
         ]);
     }
